ddbe64790b93f14b2561f9af182aa0432e021a44
oddzye <tzariov.dmitry@yandex.ru>
2020-02-25 01:47:51 +0300

    task solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return arguments.length && array.length ? array.sort((a, b) => a - b)[0] : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return arguments.length && array.length ? array.sort((a, b) => a - b)[array.length - 1] : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return arguments.length && array.length ? array.reduce((acc, number) => acc + number, 0)/array.length : 0;
 }
